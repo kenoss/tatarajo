e5192603d08dac95c59ae706cfae06490246c377
kenoss <keno.ss57@gmail.com>
2024-11-22 00:49:53 +0900

    refactor: Introduce FlexibleTimerController
    This patch introduces `FlexibleTimerController` and rewrites
    `RenderLoop` by using it.

--- crates/sabiniwm/src/render_loop.rs
+++ crates/sabiniwm/src/render_loop.rs
@@ -1,88 +1,219 @@
 use smithay::reexports::calloop::timer::{TimeoutAction, Timer};
 use smithay::reexports::calloop::{LoopHandle, RegistrationToken};
-use std::cell::{RefCell, RefMut};
+use std::cell::RefCell;
 use std::rc::Rc;
 use std::time::{Duration, Instant};
 
-pub(crate) struct RenderLoop<State> {
+/// Flexiblly reschedulable timer, without reregistering.
+///
+/// By default, it calls the given callback just once when it is started. If one calls
+/// `FlexibleTimerController::schedule_next()`, it will do that again. One can consider this as
+/// repeated timer callback with variable duration defaulting to pause.
+///
+/// Comparison:
+///
+/// - `calloop::timer::Timer`: It allows rescheduling only in the callback.
+/// - `calloop::transient::Transient<Timer>`: It needs reregistering.
+///
+/// It is used to construct `RenderLoop` that cares VBlank.
+struct FlexibleTimerController<State> {
     loop_handle: LoopHandle<'static, State>,
+    /// A timer calls `Self::callback()` (outer callback), which calls `inner_callback`.
     #[allow(clippy::type_complexity)]
-    callback: Rc<Box<dyn Fn(&mut State)>>,
-    inner: Rc<RefCell<Inner>>,
+    inner_callback: Rc<Box<dyn Fn(&mut State)>>,
+    /// A struct shared with outer callback.
+    timer_state: Rc<RefCell<FlexibleTimerState>>,
+    /// `Some` iff a timer is registered.
+    registration_token: Option<RegistrationToken>,
 }
 
-struct Inner {
-    mode: LoopMode,
-    /// Unit: 0.001Hz. E.g. about 60000 for 60Hz.
-    refresh_rate: u32,
+struct FlexibleTimerState {
+    is_running: bool,
+    committed: ScheduleInfo,
+    /// `Some` iff the current thread is in the `inner_callback`.
+    pending: Option<ScheduleInfo>,
 }
 
-/// Represents loop's current mode.
-///
-/// Note that we need to manage a timer (corresponding to `registration_token`). A timer can be
-/// dorroped:
-///
-/// - At the end of the callback, we need to choose to reregister or drop it. We can't keep it
-///   without an explicit deadline/duration. We might use very huge duration to "pause" the loop
-///   (especially, for WaitingVBlank), but we manage timers explicitly as the cost of
-///   `LoopHandle::insert_source()` is very low. (See calloop's internal.)
-/// - When the user of `RenderLoop` explicitly called `RenderLoop::stop()`.
-#[derive(Debug, PartialEq, Eq)]
-enum LoopMode {
-    /// Stopped the loop.
-    ///
-    /// Needs explicit restart.
-    Stopped,
-    /// Starting/continuing the loop and waiting for the next timer event.
-    WaitingForTimer {
-        registration_token: RegistrationToken,
-    },
-    /// Callback is started.
-    CallbackStarted {
-        registration_token: RegistrationToken,
-    },
-    /// Rendered but not scanned-out.
-    ///
-    /// It will become `WaitingForTimer` with the given deadline at the end of loop.
-    ContinueWithRefreshRate {
-        registration_token: RegistrationToken,
-        deadline: Instant,
-    },
-    /// Waiting for a VBlank, which will restart the loop.
-    WaitingForVBlank,
+struct ScheduleInfo {
+    deadline: Option<Instant>,
 }
 
-impl LoopMode {
-    fn transit(&mut self, next: LoopMode) {
-        assert!(
-            self.is_admissible_transition(&next),
-            "LoopMode::transit({:?}, {:?}) is forbidden",
-            self,
-            next
-        );
+#[derive(Debug, thiserror::Error)]
+enum FlexibleTimerControllerStartError {
+    #[error("already running")]
+    AlreadyRunning,
+}
 
-        *self = next;
-    }
+#[derive(Debug, thiserror::Error)]
+enum FlexibleTimerControllerScheduleError {
+    #[error("not running")]
+    NotRunning,
+}
 
-    fn is_admissible_transition(&self, next: &LoopMode) -> bool {
-        use LoopMode::*;
-
-        #[allow(clippy::match_like_matches_macro)]
-        match (self, next) {
-            (Stopped, WaitingForTimer { .. }) => true,
-            (WaitingForTimer { .. }, Stopped | CallbackStarted { .. }) => true,
-            (
-                CallbackStarted { .. },
-                Stopped
-                | CallbackStarted { .. }
-                | ContinueWithRefreshRate { .. }
-                | WaitingForVBlank,
-            ) => true,
-            (ContinueWithRefreshRate { .. }, Stopped | WaitingForTimer { .. }) => true,
-            (WaitingForVBlank, Stopped | WaitingForTimer { .. }) => true,
-            _ => false,
+impl<State> FlexibleTimerController<State>
+where
+    State: 'static,
+{
+    pub fn new<F>(loop_handle: LoopHandle<'static, State>, callback: F) -> Self
+    where
+        F: Fn(&mut State) + 'static,
+    {
+        let timer_state = FlexibleTimerState {
+            is_running: false,
+            committed: ScheduleInfo { deadline: None },
+            pending: None,
+        };
+        let timer_state = Rc::new(RefCell::new(timer_state));
+        Self {
+            loop_handle,
+            inner_callback: Rc::new(Box::new(callback)),
+            timer_state,
+            registration_token: None,
         }
     }
+
+    #[inline]
+    #[allow(clippy::type_complexity)]
+    fn callback(
+        timer_state: &Rc<RefCell<FlexibleTimerState>>,
+        inner_callback: &Rc<Box<dyn Fn(&mut State)>>,
+        state: &mut State,
+    ) -> TimeoutAction {
+        {
+            let mut timer_state = timer_state.borrow_mut();
+
+            if !timer_state.is_running {
+                return TimeoutAction::Drop;
+            }
+
+            timer_state.pending = Some(ScheduleInfo { deadline: None });
+        }
+
+        inner_callback(state);
+
+        {
+            let mut timer_state = timer_state.borrow_mut();
+
+            if !timer_state.is_running {
+                return TimeoutAction::Drop;
+            }
+
+            timer_state.committed = timer_state.pending.take().unwrap();
+
+            if let Some(deadline) = timer_state.committed.deadline {
+                TimeoutAction::ToInstant(deadline)
+            } else {
+                TimeoutAction::Drop
+            }
+        }
+    }
+
+    /// Starts a loop.
+    ///
+    /// Returns an error if it is already running. Callers can ignore it if it doesn't care about it.
+    pub fn start(&mut self) -> Result<(), FlexibleTimerControllerStartError> {
+        {
+            let mut timer_state = self.timer_state.borrow_mut();
+
+            if timer_state.is_running {
+                return Err(FlexibleTimerControllerStartError::AlreadyRunning);
+            }
+
+            timer_state.is_running = true;
+        }
+
+        assert!(self.timer_state.borrow().committed.deadline.is_none());
+        assert!(self.timer_state.borrow().pending.is_none());
+        assert!(self.registration_token.is_none());
+
+        self.schedule_next_aux(Instant::now());
+
+        Ok(())
+    }
+
+    /// Stops the loop.
+    ///
+    /// Note that it does nothnig and quietly returns if it is not running.
+    pub fn stop(&mut self) {
+        {
+            let mut timer_state = self.timer_state.borrow_mut();
+
+            if !timer_state.is_running {
+                return;
+            }
+
+            timer_state.is_running = false;
+            timer_state.committed.deadline = None;
+            timer_state.pending = None;
+        }
+
+        if let Some(registration_token) = self.registration_token.take() {
+            self.loop_handle.remove(registration_token);
+        }
+    }
+
+    /// Schedules next callback to be called at `deadline`.
+    ///
+    /// Returns an error if it is not running.
+    ///
+    /// If it has already scheduled one, it updates that.
+    pub fn schedule_next(
+        &mut self,
+        deadline: Instant,
+    ) -> Result<(), FlexibleTimerControllerScheduleError> {
+        if !self.timer_state.borrow().is_running {
+            return Err(FlexibleTimerControllerScheduleError::NotRunning);
+        }
+
+        {
+            let mut timer_state = self.timer_state.borrow_mut();
+            // If it is called in the `inner_callback`.
+            if let Some(pending) = timer_state.pending.as_mut() {
+                pending.deadline = Some(deadline);
+                return Ok(());
+            }
+        }
+
+        if let Some(registration_token) = self.registration_token.take() {
+            self.loop_handle.remove(registration_token);
+        }
+
+        self.schedule_next_aux(deadline);
+
+        Ok(())
+    }
+
+    fn schedule_next_aux(&mut self, deadline: Instant) {
+        assert!(self.registration_token.is_none());
+
+        {
+            let mut timer_state = self.timer_state.borrow_mut();
+            assert!(timer_state.is_running);
+            timer_state.committed.deadline = Some(deadline);
+            assert!(timer_state.pending.is_none());
+        }
+
+        // TODO: Update self.timer_state.committed?
+
+        let timer = Timer::from_deadline(deadline);
+        let timer_state = self.timer_state.clone();
+        let inner_callback = self.inner_callback.clone();
+        let registration_token = self.loop_handle
+            .insert_source(timer, move |_, _, state| {
+                Self::callback(&timer_state, &inner_callback, state)
+            })
+            .unwrap(/* safety: Registration of `Timer` never fails. */);
+        self.registration_token = Some(registration_token);
+    }
+}
+
+/// Repeated timer callback with variable duration for render loop.
+///
+/// This is built on top of `FlexibleTimerController`.
+pub(crate) struct RenderLoop<State> {
+    timer: FlexibleTimerController<State>,
+    /// Unit: 0.001Hz. E.g. about 60000 for 60Hz.
+    refresh_rate: u32,
 }
 
 impl<State> RenderLoop<State>
@@ -102,19 +233,15 @@ where
             .map(|mode| mode.refresh)
             .unwrap_or(60_000)
             .try_into()
-            // Refresh rate is positive.
-            .unwrap();
+            .unwrap(/* Refresh rate is positive. */);
         // The unit is 0.001Hz. Check the value is in 0.5Hz -- 500Hz.
         assert!(500 < refresh_rate && refresh_rate < 500_000);
 
-        let inner = Inner {
-            mode: LoopMode::Stopped,
-            refresh_rate,
-        };
+        let timer = FlexibleTimerController::new(loop_handle, callback);
+
         Self {
-            loop_handle,
-            callback: Rc::new(Box::new(callback)),
-            inner: Rc::new(RefCell::new(inner)),
+            timer,
+            refresh_rate,
         }
     }
 
@@ -127,159 +254,41 @@ where
     where
         F: Fn(&mut State) + 'static,
     {
-        let inner = Inner {
-            mode: LoopMode::Stopped,
-            refresh_rate,
-        };
+        let timer = FlexibleTimerController::new(loop_handle, callback);
+
         Self {
-            loop_handle,
-            callback: Rc::new(Box::new(callback)),
-            inner: Rc::new(RefCell::new(inner)),
-        }
-    }
-
-    #[inline]
-    #[allow(clippy::type_complexity)]
-    fn loop_(
-        inner: &Rc<RefCell<Inner>>,
-        callback: &Rc<Box<dyn Fn(&mut State)>>,
-        state: &mut State,
-    ) -> TimeoutAction {
-        {
-            let mut inner = inner.borrow_mut();
-            let LoopMode::WaitingForTimer { registration_token } = inner.mode else {
-                unreachable!();
-            };
-            inner
-                .mode
-                .transit(LoopMode::CallbackStarted { registration_token });
-        }
-
-        callback(state);
-
-        {
-            let mut inner = inner.borrow_mut();
-            match inner.mode {
-                LoopMode::Stopped => TimeoutAction::Drop,
-                LoopMode::WaitingForTimer { .. } => unreachable!(),
-                LoopMode::CallbackStarted { .. } => {
-                    inner.mode.transit(LoopMode::WaitingForVBlank);
-                    TimeoutAction::Drop
-                }
-                LoopMode::ContinueWithRefreshRate {
-                    registration_token,
-                    deadline,
-                } => {
-                    inner
-                        .mode
-                        .transit(LoopMode::WaitingForTimer { registration_token });
-                    TimeoutAction::ToInstant(deadline)
-                }
-                LoopMode::WaitingForVBlank => unreachable!(),
-            }
+            timer,
+            refresh_rate,
         }
     }
 
     pub fn start(&mut self) {
-        let timer = Timer::from_deadline(Instant::now());
-        self.start_aux(timer);
+        self.timer.start().unwrap();
     }
 
-    fn start_aux(&mut self, timer: Timer) {
-        match self.inner.borrow().mode {
-            LoopMode::Stopped | LoopMode::WaitingForVBlank => {}
-            LoopMode::WaitingForTimer { .. }
-            | LoopMode::CallbackStarted { .. }
-            | LoopMode::ContinueWithRefreshRate { .. } => return,
-        }
-
-        let inner = self.inner.clone();
-        let callback = self.callback.clone();
-        let registration_token = self
-            .loop_handle
-            .insert_source(timer, move |_, _, state| {
-                Self::loop_(&inner, &callback, state)
-            })
-            .unwrap();
-
-        let mut inner = self.inner.borrow_mut();
-        inner
-            .mode
-            .transit(LoopMode::WaitingForTimer { registration_token });
-    }
-
-    #[allow(unused)]
     pub fn stop(&mut self) {
-        let mut inner = self.inner.borrow_mut();
-        match inner.mode {
-            LoopMode::Stopped => {}
-            LoopMode::WaitingForTimer { registration_token } => {
-                self.loop_handle.remove(registration_token);
-                inner.mode.transit(LoopMode::Stopped);
-            }
-            LoopMode::CallbackStarted { .. } | LoopMode::ContinueWithRefreshRate { .. } => {
-                // The timer is dropped at the next loop end.
-                inner.mode.transit(LoopMode::Stopped);
-            }
-            LoopMode::WaitingForVBlank => {
-                inner.mode.transit(LoopMode::Stopped);
-            }
-        }
+        self.timer.stop();
     }
 
     pub fn on_render_frame(&mut self, should_schedule_render: bool) {
-        let mut inner = self.inner.borrow_mut();
-
-        let registration_token = match inner.mode {
-            LoopMode::Stopped | LoopMode::WaitingForTimer { .. } | LoopMode::WaitingForVBlank => {
-                unreachable!("on_render_frame() should be called in RenderLoop::loop_()");
-            }
-            LoopMode::CallbackStarted { registration_token } => registration_token,
-            LoopMode::ContinueWithRefreshRate { .. } => {
-                unreachable!(
-                    "on_render_frame() should be called exactly once in RenderLoop::loop_()"
-                );
-            }
-        };
-
-        if should_schedule_render {
-            // If not submitted, continue the loop.
-            //
-            // TODO: Pause the loop if no need to render.
-
-            let deadline = Self::next_deadline(&inner).unwrap();
-            inner.mode.transit(LoopMode::ContinueWithRefreshRate {
-                registration_token,
-                deadline,
-            });
+        if !should_schedule_render {
+            return;
         }
+
+        // If scanout is not done, continue the loop.
+        //
+        // TODO: Pause the loop if no need to render.
+
+        let deadline = self.next_deadline();
+        let _ = self.timer.schedule_next(deadline);
     }
 
     pub fn on_vblank(&mut self) {
-        let timer = {
-            let inner = self.inner.borrow_mut();
-
-            match inner.mode {
-                // It is possible that `.stop()` is called before it, e.g. by
-                // `smithay::backend::session::Event::SessionEvent::PauseSession`.
-                LoopMode::Stopped => return,
-                // IIUC, this can be occur in the initial rendering.
-                //
-                // TODO: Investigate it.
-                LoopMode::WaitingForTimer { .. } => return,
-                LoopMode::WaitingForVBlank => {}
-                LoopMode::CallbackStarted { .. } | LoopMode::ContinueWithRefreshRate { .. } => {
-                    unreachable!("on_vblank() should follow on_render_frame(false)");
-                }
-            };
-
-            let deadline = Self::next_deadline(&inner).unwrap();
-            Timer::from_deadline(deadline)
-        };
-        self.start_aux(timer);
+        let deadline = self.next_deadline();
+        let _ = self.timer.schedule_next(deadline);
     }
 
-    fn next_deadline(inner: &RefMut<'_, Inner>) -> Option<Instant> {
+    fn next_deadline(&self) -> Instant {
         // TODO:
         //
         // - Subtract a duration for sabiniwm's render so that we can submit a next frame before
@@ -287,8 +296,10 @@ where
         //   https://github.com/Smithay/smithay/blob/8e49b9bb1849f0ead1ba2c7cd76802fc12ad6ac3/anvil/src/udev.rs#L1305
         // - Use `last_render_ended_at` for base point.
         let duration =
-            Duration::from_micros((1_000_000f32 * 1000.0 / inner.refresh_rate as f32) as u64);
-        Instant::now().checked_add(duration)
+            Duration::from_micros((1_000_000f32 * 1000.0 / self.refresh_rate as f32) as u64);
+        Instant::now()
+            .checked_add(duration)
+            .expect("std::time::Instant doesn't overflow")
     }
 }
 
